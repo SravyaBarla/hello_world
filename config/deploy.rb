c72a957d7e04325a6a28bb41c02e9b32cf5b7943
SravyaBarla <sravya.b@westagilelabs.com>
2022-04-04 18:58:29 +0530

    second

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,11 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.17.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "hello_world"
+set :repo_url, "git@github.com:SravyaBarla/hello_world.git"
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :rvm_ruby_version, 'ruby 2.7.4'
+set :passenger_restart_with_touch, true
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,3 +59,11 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+role :app, %w{deploy_user@3.35.19.213}
+role :web, %w{deploy_user@3.35.19.213}
+role :db,  %w{deploy_user@3.35.19.213}
+set :ssh_options, {
+   keys: %w(C:\Users\Sai Sravya\Downloads\example-devops.pem),
+   forward_agent: false,
+   auth_methods: %w(ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCuMGF/YE52Z5aC/WbkpEW46yDGBwiXE53L4vTDknftEPnmGI8l3A002gdD2VR9LjhukFmfHQZcyi56ADAclDPNIKCGNfI8sJK1KnONd7+eqXgClqZp6TKRvnDkQGutgcBO9ZUFqNiBTB8IG1kht/FCJVtkcQ3kitEbaVMGY3Dmv5X2HojhBMX4afgEOzW2hoLzrZAqyq7SlFSRedCJ5+UTKP0Fv7M+hpkGSjTh1uVV8ftwHNz1YTK9KWZbHyOO1KH6crbiVvYh0kgBdXwoNE8b2d/Pp0uS90XwJ2bWSdJnPbLLhWHQBdWMe0HqT4yMEZZaH5EEndCRXXiULM3RW7u5hfLftW7l3ml1rJ3Ojs/O8cRtmiC9KMBPPjO26EqFhRrxktVxJkleBwwWd8dTi/uF+pv6T50sSKUFJICYCHqBdtWHcWC7e9whc5EAzjIEdd5bUWS9z0B6Jg87wgXWwFLZAESNH9PKCGPmcvJifg7nlk269wrtLejY/oE6/TmLJRU= sai sravya@LAPTOP-824UOCAT)
+ }
